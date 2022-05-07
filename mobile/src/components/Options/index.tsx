b5f02eefdb40568cbb68826421a67a32e493874c
Andreia Souza <fsouza.andreia@gmail.com>
2022-05-07 16:44:25 -0300

    adding functionality to components and making server connection

--- mobile/src/components/Options/index.tsx
+++ mobile/src/components/Options/index.tsx
@@ -3,12 +3,16 @@ import { View, Text } from 'react-native';
 
 import { Copyright } from '../Copyright';
 import { Option } from '../Option';
+import { FeedbackType } from '../Widget'
 
 import { feedbackTypes } from '../../utils/feedbackTypes'
 import { styles } from './styles';
 
+interface Props {
+  onFeedbackTypeChanged: (feedbackType: FeedbackType) => void;
+}
 
-export function Options() {
+export function Options({onFeedbackTypeChanged}: Props) {
   return (
     <View style={styles.container}>
 
@@ -22,6 +26,7 @@ export function Options() {
             key={key}
             title={value.title}
             image={value.image}
+            onPress={() => onFeedbackTypeChanged(key as FeedbackType)}
           />
         ))}
       </View>
